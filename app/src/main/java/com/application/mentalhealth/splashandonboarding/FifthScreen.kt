53007280a16f0349e38a0f8d0b4fd4a3cc0209eb
Nishant Rishab <imreprisal@gmail.com>
2021-11-09 17:02:46 +0530

    commit............ not decided ................

--- app/src/main/java/com/application/mentalhealth/splashandonboarding/FifthScreen.kt
+++ app/src/main/java/com/application/mentalhealth/splashandonboarding/FifthScreen.kt
@@ -1,27 +1,19 @@
 package com.application.mentalhealth.splashandonboarding
-
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import androidx.viewpager2.widget.ViewPager2
 import com.application.mentalhealth.R
 import kotlinx.android.synthetic.main.fragment_fifth_screen.view.*
 
-class FifthScreen : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_fifth_screen, container, false)
+class FifthScreen : Fragment(R.layout.fragment_fifth_screen) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        val viewPager =  activity?.findViewById<ViewPager2>(R.id.viewPager)
-
-        view.btnNext5.setOnClickListener {
+        val viewPager = activity?.findViewById<ViewPager2>(R.id.viewPager)
+        view.btnNext511.setOnClickListener {
             viewPager?.currentItem = 6
         }
-        return view
     }
 }
--- app/src/main/java/com/application/mentalhealth/splashandonboarding/FourthScreen.kt
+++ app/src/main/java/com/application/mentalhealth/splashandonboarding/FourthScreen.kt
@@ -1,15 +1,16 @@
 package com.application.mentalhealth.splashandonboarding
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import androidx.fragment.app.Fragment
 import android.view.View
 import android.view.ViewGroup
 import androidx.viewpager2.widget.ViewPager2
 import com.application.mentalhealth.R
 import kotlinx.android.synthetic.main.fragment_fourth_screen.view.*
 
-class FourthScreen : Fragment() {
+class FourthScreen : Fragment(R.layout.fragment_fourth_screen) {
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
