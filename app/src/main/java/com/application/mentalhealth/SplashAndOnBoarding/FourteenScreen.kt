9675e0d7584908e03639d51fb05dc193683f7579
sudararshan <sudarshanprasad18@gmail.com>
2021-11-12 13:22:58 +0530

    Merge Done

--- app/src/main/java/com/application/mentalhealth/SplashAndOnBoarding/FourteenScreen.kt
+++ app/src/main/java/com/application/mentalhealth/SplashAndOnBoarding/FourteenScreen.kt
@@ -1,12 +1,24 @@
 package com.application.mentalhealth.SplashAndOnBoarding
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.application.mentalhealth.R
 
 class FourteenScreen : Fragment(R.layout.fragment_fourteen_screen) {
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val handler : Handler = Handler()
+        handler.postDelayed(Runnable {
+            kotlin.run {
+                findNavController().navigate(R.id.action_fourteenScreen_to_homeActivity)
+            }
+        },2000)
+
+    }
 }
--- app/src/main/java/com/application/mentalhealth/SplashAndOnBoarding/SplashFragment.kt
+++ app/src/main/java/com/application/mentalhealth/SplashAndOnBoarding/SplashFragment.kt
@@ -6,8 +6,9 @@ import android.os.Handler
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.application.mentalhealth.R
+
 import com.bumptech.glide.Glide
+import com.application.mentalhealth.R
 import kotlinx.android.synthetic.main.fragment_splash.*
 
 class SplashFragment : Fragment(R.layout.fragment_splash) {
