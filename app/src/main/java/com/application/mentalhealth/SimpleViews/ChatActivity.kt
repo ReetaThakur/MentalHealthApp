1a2b3dc03b5fb5a6e14e25d36329b8b3db677571
Nishant Rishab <imreprisal@gmail.com>
2021-11-13 15:12:33 +0530

    chat page done...................not last

--- app/src/main/java/com/application/mentalhealth/SimpleViews/ChatActivity.kt
+++ app/src/main/java/com/application/mentalhealth/SimpleViews/ChatActivity.kt
@@ -3,10 +3,13 @@ package com.application.mentalhealth.SimpleViews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.application.mentalhealth.R
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_chat.*
 
-class chatActivity : AppCompatActivity() {
+class ChatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
+        Glide.with(this).asGif().load(R.raw.let_us_chat).into(ivChatGifView)
     }
 }
